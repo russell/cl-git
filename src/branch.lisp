f0ae1cb47d18468a4fc290a0468899fc29dfe4ec
Russell Sim <russell.sim@gmail.com>
2013-05-15 21:06:56 +1000

    Whitespace cleanup.

--- src/branch.lisp
+++ src/branch.lisp
@@ -84,9 +84,9 @@ This means that this is the branch that is checked out."
   "Returns a list of all branche names in the repository.  Each
 branch is represented as cons pair (branch-name . type-flags)
 
-The type flags are :LOCAL and :REMOTE.  
+The type flags are :LOCAL and :REMOTE.
 
-By default all the branches, both remote and local are returned.  
+By default all the branches, both remote and local are returned.
 It is possible to limit the list of branches to just the local or remote by
 specifying :LOCAL or :REMOTE as argument to the :TYPE parameter.
 "
@@ -102,7 +102,7 @@ specifying :LOCAL or :REMOTE as argument to the :TYPE parameter.
                (name string)
                &key (repository *git-repository*)
              (type :local))
-  "Converts a branch name to a git REFERENCE.  The optional :type argument 
+  "Converts a branch name to a git REFERENCE.  The optional :type argument
 specifies if the branch name names a local branch or a remote branch."
   (with-foreign-object (reference :pointer)
     (%git-branch-lookup reference repository name type)
@@ -118,14 +118,14 @@ specifies if the branch name names a local branch or a remote branch."
 The branch-name-and-type should be a cons pair like
 \(branch-name branch-type\).
 
-This is the same format as the elements returned by the 
+This is the same format as the elements returned by the
 
 \(git-list :branch ...\)
 
 call.
 "
-  (git-lookup class (car branch-name-and-type) 
-	      :repository repository 
+  (git-lookup class (car branch-name-and-type)
+	      :repository repository
 	      :type (cdr branch-name-and-type)))
 
 
@@ -142,7 +142,7 @@ branch BRANCH."
 
 (defmethod git-upstream-name ((branch-name string) &key (repository *git-repository*))
   (with-foreign-pointer-as-string ((out size) (%git-branch-upstream-name
-					       (null-pointer) 0 
+					       (null-pointer) 0
 					       repository
 					       branch-name))
         (%git-branch-upstream-name out size repository branch-name)))
@@ -150,8 +150,7 @@ branch BRANCH."
 
 (defmethod git-remote-name ((branch-name string) &key (repository *git-repository*))
   (with-foreign-pointer-as-string ((out size) (%git-branch-remote-name
-					       (null-pointer) 0 
+					       (null-pointer) 0
 					       repository
 					       branch-name))
         (%git-branch-remote-name out size repository branch-name)))
-
